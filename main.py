17fc5a3c3e689a822baa8c438c13ad3f331aadf8
Игорь <kolifan@greenmile.ru>
2026-06-09 10:28:00 +0300

    Update the split-section method according to new specification

--- main.py
+++ main.py
@@ -4,21 +4,31 @@ def cage(array, num):
         return -1
 
     nindexstart = 0
+    retindex = 0
     nindexfin = len(array) - 1
+    moreloop = True
 
-    while nindexstart <= nindexfin:
+    while nindexstart <= nindexfin and moreloop:
         nindex = (nindexfin + nindexstart) // 2
         if array[nindex] == num:
-            return nindex
-
-        if array[nindex] < num:
+            retindex = nindex
+            moreloop = False
+        elif array[nindex] < num:
             nindexstart = nindex + 1
         else:
             nindexfin = nindex - 1
 
-    return -1
+    delta = nindexstart - nindexfin
+    if delta==1:
+        return -1 - nindexstart
+    elif delta==-1:
+        return retindex
+    else:
+        while array[retindex]==num and retindex>0:
+            retindex = retindex - 1
+        return retindex + 1
 
-a = [2,3,5,7,11,14,19,21,32,41,43]
+a = [2,2,3,7,7,11,15,21,21,21,35,43]
 lion = input("inpit number: ")
 ind = cage(a, int(lion))
 print(f"index of {lion} is {ind}")
